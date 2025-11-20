829b1a5563542af6e2ecbfe27d4c936177f08348
Quan <havietquan2004@gmail.com>
2025-11-20 22:16:08 +0700

    Update

--- master/generator.py
+++ master/generator.py
@@ -1,10 +1,38 @@
+import numpy as np
+import psycopg2
+from langchain_classic.chains import create_retrieval_chain
 import torch
 from transformers import AutoTokenizer, pipeline
 
-llm_name = "meta-llama/Llama-3.2-3B-Instruct"
-pipe = pipeline(
-    "text-generation",
-    model=llm_name,
-    torch_dtype = torch.float16,
-    device_map = 'cuda',
-)
+class Generator_llm:
+    def __init__(self):
+        self.llm_name = "meta-llama/Llama-3.2-3B-Instruct"
+        self.pipe = pipeline(
+            "text-generation",
+            model=self.llm_name,
+            torch_dtype=torch.float16,
+            device_map='cuda',
+        )
+
+def build_context(products):
+    context = "\n".join([
+        f"- {p[0]} (${p[2]}): {p[1]}"
+        for p in products
+    ])
+    return context
+
+def build_messages(context, query):
+    messages = [
+        {"role": "system", "content": "You are a helpful flower shop assistant."
+                    "Provide one Answer ONLY the following query based on the context provided below. "
+                    "Do not generate or answer any other questions. "
+                    "Do not make up or infer any information that is not directly stated in the context. "
+                    "Provide a concise answer."
+                    f"{context}"},
+        {"role": "user", "content": query},
+    ]
+    return messages
+
+def generate_reponse(self, messages):
+    response = self.pipe(messages, max_new_tokens=128)[-1]["generated_text"][-1]["content"]
+    return response
--- master/retrieval.py
+++ master/retrieval.py
@@ -1,19 +1,28 @@
 import numpy as np
 import psycopg2
-
 from master.connect import connect
+from transformers import AutoModel
+import torch
+from huggingface_hub import notebook_login
+import ast
 
+class Embed_llm:
+    def __init__(self):
+        hf_token = "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"
+        notebook_login()
+        self.model = AutoModel.from_pretrained("jinaai/jina-embeddings-v4", trust_remote_code=True, dtype = torch.float16)
+        self.model.to("cuda" if torch.cuda.is_available() else "cpu")
 
 def cosine_similarity(a, b):
     a = np.array(a)
     b = np.array(b)
     return np.dot(a, b.T) / (np.linalg.norm(a, axis=1)[:, None] * np.linalg.norm(b, axis=1))
 
-import ast
-def retrieval_vector(query, model):
+
+def retrieval_vector(query,self):
     try:
         cur, conn = connect()
-        query_embedding = np.array(model.encode_text(
+        query_embedding = np.array(self.model.encode_text(
             texts = query,
             task = "retrieval",
             return_numpy = True,
@@ -47,4 +56,36 @@ def retrieval_vector(query, model):
         cur.close()
         print('Database connection closed.')
 
+    return 0
+
+def read_column(cur):
+    cur.execute('select id, name, description, price, image_url from Flower where vector IS NULL')
+    if cur is None:
+        print('No data found')
+    return cur
+
+def embedded_all_column(cur, conn, self):
+    try:
+        rows = cur.fetchall()
+        print(f"🧩 Found {len(rows)} rows to embed...")
+        for row in rows:
+            flower_id, name, description, price, image = row
+
+            text = text = f"{name}. {description or ''}. Price: ${price:.2f}"
+
+            text_embedding = self.model.encode_text(
+                texts = text,
+                task = "retrieval",
+                return_numpy = True,
+            )
+            text_embedding = text_embedding.squeeze().astype(np.float16).tolist()
+
+            cur.execute('Update Flower Set vector = %s where id = %s', (text_embedding, flower_id))
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        conn.commit()
+        conn.close()
+        cur.close()
+        print('Database connection closed.')
     return 0
