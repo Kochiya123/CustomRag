b6bad3ce8a2ca27fc25d83478e50c59583bf505e
Quan <havietquan2004@gmail.com>
2026-01-11 15:59:33 +0700

    Update to prompt

--- master/generate_text_module/generator.py
+++ master/generate_text_module/generator.py
@@ -82,8 +82,7 @@ def build_message(context, prompt_input, image_link, chat_history):
         f"If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. "
         f"If you don't know the answer to a question, please response as language model you are not able to respone detailed to these kind of question."
         f"Old chat history for reference: {chat_history}"
-        f"Create a link to the product with the following format https://flower-plus.vercel.app/product/product_id where replace the product_id with the actual product ID"
-        f"If include, link must be clickable"
+        f"Create a website link to the product with the following format https://flower-plus.vercel.app/product/product_id where replace the product_id with the actual product ID"
     )
 
     # Initialize the user content list
